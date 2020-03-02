88c25a270d4c521d14ca9d84204f02f9448ad07a
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-03-02 16:12:10 +0100

    Fix: forgor to push header

---
+++ libvla.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libvla.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 11:39:51 by aaugusti          #+#    #+#             */
+/*   Updated: 2020/03/02 14:43:30 by aaugusti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBVLA_H
+# define LIBVLA_H
+
+# include <stddef.h>
+# include <stdbool.h>
+
+typedef struct	s_vla {
+	size_t	elem_sz;
+	size_t	size;
+	size_t	cap;
+	void	*vla;
+}				t_vla;
+
+void	*vla_memcpy(void *dst, const void *src, size_t n);
+bool	vla_init(size_t elem_sz, size_t init_cap, t_vla *result);
+bool	vla_push(t_vla *vla, void *elem);
+bool	vla_shrink(t_vla *vla);
+bool	vla_resize(t_vla *vla);
+bool	vla_get(t_vla vla, size_t index, void *result);
+bool	vla_set(t_vla vla, size_t index, void *new_value);
+
+#endif
